d81f166c1c179aa70763c742ed15cf9bbe1af730
Maximosleo <134728581+Maximosleo@users.noreply.github.com>
2023-06-03 21:57:57 +0300

    Sign up client get json response from the the server
    SI

--- Pages/SignUp.cshtml.cs
+++ Pages/SignUp.cshtml.cs
@@ -1,43 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Caching.Memory;
-using System.Data;
-using System.Diagnostics.Metrics;
-using System.Text.Json;
 
 namespace OurProject2.Pages
 {
     public class SignUpModel : PageModel
     {
-        private readonly IMemoryCache _cache;
+        private readonly IMemoryCache _cache11;
 
+        // constructor
         public SignUpModel(IMemoryCache cache)
         {
-            _cache = cache;
+            _cache11 = cache;
         }
-        public IActionResult OnGet(string firstname, string lastname, string email, string password, string gender, string age,string identification, string admin_user)
+        public IActionResult OnGet(string firstname, string lastname, string email,
+            string password, string gender, string age,string identification, string admin_user)
         {
-
-
             // Set response
+            // if this code is called from a button click in the client
             if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
             {
-                /*              var counter = _cache.GetOrCreate("Counter", entry => 0);
-                              counter++;
-                              _cache.Set("Counter", counter);
-
-                              Random random = new Random();
-                              int session = random.Next(1, 10001); // The upper bound is exclusive, so we use 10001 to include 10000
-                              _cache.Set(counter + "", session + "");*/
-
-                // Log inputs
-                //Console.WriteLine("First name : " + firstname);
-                //Console.WriteLine("Last Name : " + lastname);
-                //Console.WriteLine("email : " + email);
-                //Console.WriteLine("password : " + password);
-                //Console.WriteLine("gender : " + gender);
-                //Console.WriteLine("age : " + age);
-
                 bool isAdmin;
                 if (admin_user == "admin")
                 {
@@ -48,36 +30,46 @@ namespace OurProject2.Pages
                     isAdmin = false;
                 }
                
-
                 SaveToDB(firstname, lastname, email, password, gender, age, identification, isAdmin);
 
-                    // Create a new anonymous object for your JSON response
-                var jsonResponse = new { isAdmin1 = isAdmin, message = "Operation successful" };
-
-                var obj = new { isAdmin = true, message = "Operation successful" };
-                string json = JsonSerializer.Serialize(obj);
-
-             //         return new JsonResult(json);
-
-
-                return Content(jsonResponse.ToString());
+                var sucess = "sucess";
+                var result = new { sucess, isAdmin};
+                var jsonResult = new JsonResult(result);
+                return jsonResult;
             }
+            // if this code is called when the client page is starting
             else
             {
                 return Page();
             }
         }
 
+
+/*        public IActionResult OnGet(string input1, string input2, string input3)
+        {
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+
+                var result = new { input1, input2, input3 };
+                Console.WriteLine($"Input1: {input1}, Input2: {input2}, Input3: {input3}");
+                return new JsonResult(result);
+            }
+            else
+            {
+                return Page();
+            }
+        }*/
+
         private void SaveToDB(string firstname, string lastname, string email, string password, string gender, string age, string identification, bool isAdmin)
         {
-            var tableJson = _cache.GetOrCreate("DB", entry => "");
+            var tableJson = _cache11.GetOrCreate("DB", entry => "");
             GlobalDataTable globalDataTable = GlobalDataTable.DeserializeFromJson(tableJson);
 
             DataTableData dataTableData = globalDataTable.DataTableData;
             dataTableData.Rows.Add(new RowData { ID = identification, Name = firstname, lastname = lastname, email = email, password = password, gender = gender, age = age, isAdmin = isAdmin });
 
             var tJson = globalDataTable.SerializeToJson();
-            _cache.Set("DB", tJson);
+            _cache11.Set("DB", tJson);
         }
     }
 }
